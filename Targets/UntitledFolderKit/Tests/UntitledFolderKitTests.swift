99ab6be23515860bd62d5f7cfefe735b2899f7be
Engin Gülek <engingulek0@gmail.com>
2023-11-08 13:03:58 +0300

    Init Tuist

---
+++ Targets/UntitledFolderKit/Tests/UntitledFolderKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class UntitledFolderKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("UntitledFolderKit", "UntitledFolderKit")
+    }
+}
---
+++ Targets/UntitledFolderUI/Tests/UntitledFolderUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class UntitledFolderUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("UntitledFolderUI", "UntitledFolderUI")
+    }
+}
